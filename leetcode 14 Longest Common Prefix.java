e4b218693f1423967c91537cca96a74025c40172
Sean <shixiaoran1989@gmail.com>
2015-06-08 00:15:45 -0700

    Create leetcode 14 Longest Common Prefix.java

---
+++ leetcode 14 Longest Common Prefix.java
@@ -0,0 +1,25 @@
+// leetcode 14 Longest Common Prefix.java
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int size = strs.length, i, j;
+        int[] sizes = new int[size];
+        String result = "";
+
+        if (size == 0) return result;
+
+        for (i = 0; i < size; i++)
+            sizes[i] = strs[i].length();
+
+        for (i = 0; ; i++) {
+            if (i >= sizes[0]) return result;
+
+            for (j = 1; j < size; j++) {
+                if (i >= sizes[j]) return result;
+                if (strs[j].charAt(i) != strs[0].charAt(i)) return result;
+            }
+
+            result += strs[0].charAt(i);
+        }
+    }
+}
